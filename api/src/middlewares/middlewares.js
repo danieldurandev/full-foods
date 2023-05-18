999cc809da83ae0e17c7764ed02b31b237482223
Daniel Durán <danielduranje@outlook.com>
2023-05-18 11:20:38 -0500

    middlewares: actualizados

--- api/src/middlewares/middlewares.js
+++ api/src/middlewares/middlewares.js
@@ -9,7 +9,7 @@ const middlewares = [
   cookieParser(),
   morgan("dev"),
   (req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "https://food-client-t5n3.onrender.com"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Credentials", "true");
     res.header(
       "Access-Control-Allow-Headers",
